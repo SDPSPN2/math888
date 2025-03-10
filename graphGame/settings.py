8000531f0209d7020ef6f0ebfcabca38cc60032e
Nicasiozero <lucifer04957@gmail.com>
2025-03-10 09:39:07 +0700

    update

--- graphGame/settings.py
+++ graphGame/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "game_main", "static"),
     os.path.join(BASE_DIR, "homepage", "static"),
     os.path.join(BASE_DIR, "lobby", "static"),
