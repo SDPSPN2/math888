c8246b78c00c8e20e0321edc3d6e9cd71535e6b3
Nicasiozero <lucifer04957@gmail.com>
2025-03-10 09:56:16 +0700

    fix csrf

--- graphGame/settings.py
+++ graphGame/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s*24v313bme9sdal@um4!t%l!!_f%)&l+p^c04n12jrmfu+l0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
